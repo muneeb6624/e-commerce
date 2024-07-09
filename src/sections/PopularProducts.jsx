d8f69507f8eae3add1be490299c68d6b28b2a83c
Muhammad Muneeb <qureshimuhammadmuneeb@gmail.com>
2024-07-09 15:34:31 +0500

    test

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -2,6 +2,7 @@ import { products } from "../constants";
 import { PopularProductCard } from "../components";
 
 const PopularProducts = () => {
+  console.log("test 123")
   return (
     <section id='products' className='max-container max-sm:mt-12'>
       <div className='flex flex-col justify-start gap-5'>
